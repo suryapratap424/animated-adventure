32f2c0216f10e2fd0f3724bbdfac457c55c09405
Surya Pratap Singh <suryaprataps424@gmail.com>
2023-02-06 19:08:25 +0530

    added graph functionality

--- graph.js
+++ graph.js
@@ -24,7 +24,7 @@ class Graph {
     ctx.fillRect(0, 0, W, H);
     // console.log(this.options);
   }
-  drawAxis(ctx, x = 100, y = 100) {
+  drawAxis(ctx, x = 10, y = 10) {
     let O = this.origin;
     let [W, H] = O.map((a) => a * 2);
     this.xScale = W / x;
@@ -35,6 +35,36 @@ class Graph {
     new Vector(0, H / 2, A).drawCut(ctx, O, this.yScale);
     new Vector(0, -H / 2, A).drawCut(ctx, O, this.yScale);
   }
+  clear(ctx){
+    ctx.fillStyle = this.options.backgroundColor;
+    ctx.fillRect(0, 0, ...this.origin.map((a) => a * 2));
+  }
+  draw(ctx, f) {
+    // this.t=ctx.canvas.clientWidth/2
+    this.v = new Vector(0, f, this.c);
+    this.v.draw(ctx, [this.t, ctx.canvas.clientHeight / 2]);
+    this.t++;
+  }
+  static getPoints(ctx, f, n) {
+    let W = ctx.canvas.clientWidth;
+    let x;
+    let point = [];
+    for (x = -W / 2; x < W / 2; x += n) {
+      try {
+        let y = eval(f);
+        y = isNaN(y) ? 0 : y;
+        y = y == Infinity ? 0 : y;
+        y = typeof y === "number" ? y : 0;
+        point.push([x, y]);
+      } catch (e) {
+        // console.log(e);
+        point.push([x, 0]);
+      }
+    }
+    return point;
+  }
+}
+class DescreteGraph extends Graph {
   drawfunction(ctx, f) {
     let [W, H] = this.origin.map((a) => a * 2);
     let max = W / (2 * this.xScale);
@@ -69,29 +99,42 @@ class Graph {
       }
     }, speed);
   }
-  draw(ctx, f) {
-    // this.t=ctx.canvas.clientWidth/2
-    this.v = new Vector(0, f, this.c);
-    this.v.draw(ctx, [this.t, ctx.canvas.clientHeight / 2]);
-    this.t++;
-  }
-  static getPoints(ctx, f, n) {
-    let W = ctx.canvas.clientWidth;
-    let x;
-    let point = [];
-    for (x = -W / 2; x < W / 2; x += n) {
-      try {
-        let y = eval(f);
-        y = isNaN(y) ? 0 : y;
-        y = y == Infinity ? 0 : y;
-        y = typeof y === "number" ? y : 0;
-        point.push([x, y]);
-      } catch (e) {
-        // console.log(e);
-        point.push([x, 0]);
+}
+class ContinuousGraph extends Graph {
+  drawfunction(ctx, f) {
+    let [W, H] = this.origin.map((a) => a * 2);
+    let max = W/2;
+    let X = -max;
+    // console.log(x, this.xScale);
+    while (X < max) {
+      let x=X/this.xScale;
+      let y = eval(f);
+      if (!isNaN(y) && typeof y === "number") {
+        let v = new Vector(0, y, this.options.functionColor);
+        v.MagOptions.is = this.options.showVals;
+        v.draw(ctx, [W / 2 + X , H / 2], this.yScale);
       }
+      X++;
     }
-    return point;
+  }
+  animatefunction(ctx, f) {
+    let [W, H] = this.origin.map((a) => a * 2);
+    let X = -W/2;
+    let anime = setInterval(() => {
+      let x=X/this.xScale;
+      let y = eval(f);
+      if (!isNaN(y) && typeof y === "number") {
+        let v = new Vector(0, y, this.options.functionColor);
+        v.MagOptions.is = this.options.showVals;
+        v.draw(ctx, [W / 2 + X, H / 2], this.yScale);
+      }
+      X++;
+      if (X > W/2) {
+        // console.log(speed * x * 2);
+        clearInterval(anime);
+      }
+    }, 5);
   }
 }
-export default Graph;
+
+export { Graph, DescreteGraph,ContinuousGraph };
